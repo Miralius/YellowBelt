ad15fdfb9a44fbd939a13e9abf10fceceefa803e
Антон Фролов <F-Miralius@yandex.ru>
2024-04-01 18:17:39 +0400

    Final: Removed redundant TODO in `main.cpp`

--- Final/main.cpp
+++ Final/main.cpp
@@ -1,7 +1,6 @@
 #include "database.h"
 #include "date.h"
 #include "condition_parser.h"
-// TODO: maybe #include "node.h" is needed here
 // The #if is needed for stepik check system submitting if we use the whole file
 #if __GNUC__ == 13 && __GNUC_MINOR__ == 2
 
