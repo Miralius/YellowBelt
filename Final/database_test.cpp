715270957a911cd80ae24e5e34272beb133b156c
Антон Фролов <F-Miralius@yandex.ru>
2024-04-01 18:38:43 +0400

    Final: Implemented test for deleting in `Database`

--- Final/database_test.cpp
+++ Final/database_test.cpp
@@ -3,7 +3,6 @@
 //
 #include "main.h"
 #include "database.h"
-#include "condition_parser.h"
 // The #if is needed for stepik check system submitting if we use the whole file
 #if __GNUC__ == 13 && __GNUC_MINOR__ == 2
 
@@ -13,24 +12,25 @@
 #include "test_runner.h"
 #endif
 
-#include <sstream>
-
 void TestDatabase() {
     {
         Database db;
-        istringstream is("Add 2017-11-07 big sport event");
-        const auto date = ParseDate(is);
-        const auto event = ParseEvent(is);
-        db.Add(date, event);
-
-        is = istringstream("date == 2017-11-07");
-        auto condition = ParseCondition(is);
-        auto predicate = [condition](const Date &date, const string &event) {
-            return condition->Evaluate(date, event);
-        };
-
-        const auto entries = db.FindIf(predicate);
+        const auto dateExample = Date(2017, 11, 7);
+        db.Add(dateExample, "big sport event");
+        const auto entries = db.FindIf([&dateExample](const Date &date, const string &) {
+            return date == dateExample;
+        });
         Assert(entries.size() == 1, "Database 'found 1 entries' after adding");
         AssertEqual(*entries.cbegin(), "big sport event", "Database checks 'Add 2017-11-07 big sport event' work");
+    }
+    {
+        Database db;
+        db.Add({2017, 6, 1}, "1st of June");
+        const auto dateExample = Date(2017, 7, 8);
+        db.Add(dateExample, "8th of July");
+        db.Add(dateExample, "Someone's birthday");
+        AssertEqual(db.RemoveIf([&dateExample](const Date &date, const string &) {
+            return date == dateExample;
+        }), 2, "Database checks 'Del date == 2017-07-08' where there are 2 entries");
     }
 }
